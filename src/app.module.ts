307d147c3edb6f0ae662e4389b5fd4ba16b63a3e
aestas <hawhawba@gmail.com>
2023-06-09 19:28:10 +0900

    overwrite with nest new

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
-import { Controller, Module, Get } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
+  imports: [],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
